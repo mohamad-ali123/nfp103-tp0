18bca66e9cc3f887cd111998440a7f8d0676bb3b
Mohamad ali <mohamad.ali.92@isae.edu.lb>
2020-05-15 23:33:46 +0300

    Add files via upload
    devoir

--- nfp103-tp0/src/main/java/lb/edu/isae/nfp103/tp0/Data.java
+++ nfp103-tp0/src/main/java/lb/edu/isae/nfp103/tp0/Data.java
@@ -4,20 +4,37 @@ https://www.gnu.org/licenses/lgpl-3.0.fr.html
  */
 package lb.edu.isae.nfp103.tp0;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author Pascal Fares <pascal.fares at cofares.net>
  */
 public class Data {
-
+	
+    Semaphore s = new Semaphore(1);
     private int x = 0;
 
     public void inc() {
-        x = x + 1;
+        try{
+	    s.acquire();
+	    x = x + 1;
+	    s.release();
+	}
+	catch(InterruptedException e){
+		e.printStackTrace();
+	}
     }
 
     public void dec() {
-        x = x - 1;
+        try{
+	    s.acquire();
+	    x = x - 1;
+	    s.release();
+	}
+	catch(InterruptedException e){
+		e.printStackTrace();
+	}
     }
     
     @Override
